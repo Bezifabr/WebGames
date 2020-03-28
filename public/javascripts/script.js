84e77ad94c5858db8623396541d522ba03b85e48
Bezifabr <bezifabr@gmail.com>
2020-03-28 15:28:18 +0100

    Implement time label script

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,7 +2,7 @@ var score  = 0;
 var aWidth;
 var aHeight;
 var timer;
-var iterations = 0;
+var timeLeft = 30;
 
 window.addEventListener('load', setGameAreaBounds);
 
@@ -15,14 +15,30 @@ function setGameAreaBounds() {
     document.getElementById('gameArea').style.width = aWidth + 'px';
     document.getElementById('gameArea').style.height = aHeight + 'px';
     document.getElementById('dot').addEventListener('click', detectHit);
-    aWidth -= 74;
-    aHeight -= 74;
-    moveDot();
+    aWidth -= 174;
+    aHeight -= 174;
+    iterate();
 }
 
 function detectHit() {
     score += 1;
     document.getElementById('scoreLabel').innerHTML = "Score: " + score;
+    moveDot();
+}
+
+function iterate() {
+
+    if(timeLeft > 0) {
+        timer = setTimeout("iterate()", 1000);
+        timeLeft--;
+        document.getElementById('timeLabel').innerHTML = "Time left: " + timeLeft;
+    }
+    else {
+        clearTimeout(timer);
+        document.getElementById('scoreLabel').innerHTML += "     GAME OVER!";
+        document.getElementById('timeLabel').innerHTML = "";
+        document.getElementById('dot').removeEventListener("click", detectHit);
+    }
 }
 
 function moveDot() {
@@ -35,12 +51,4 @@ function moveDot() {
 
     document.getElementById('dot').style.left = x + 'px';
     document.getElementById('dot').style.top = y + 'px';
-    if(iterations < 30)
-        timer = setTimeout("moveDot()", 1000);
-    else{
-        document.getElementById('scoreLabel').innerHTML += "     GAME OVER!";
-        document.getElementById('dot').removeEventListener("click", detectHit);
-        clearTimeout(timer);
-    }
-    iterations++;
 }
