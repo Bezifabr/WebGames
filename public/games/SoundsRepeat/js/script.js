5f10c9c715477b59bfca00e0dae730a89adab864
Bezifabr <bezifabr@gmail.com>
2020-04-02 15:49:21 +0200

    Refactor PlaySound() method

--- public/games/SoundsRepeat/js/script.js
+++ public/games/SoundsRepeat/js/script.js
@@ -1,24 +1,23 @@
 
-var isPlaying = false;
+var gameOver = false;
 
 function PlaySound(padId) {
-    if(isPlaying == false) {
-        var snd = new Audio("../wav/" + padId + ".wav");
-        var pad = document.getElementById(padId);
-        snd.volume = 0.2;
-        snd.play();
+    if (gameOver == false) {
+        var padSound = new Audio("../wav/pad"+padId + ".wav");
+        var pad = document.getElementById("pad" + padId);
+        padSound.currentTime = 0;
+        padSound.volume = 0.2;
+        padSound.play();
         pad.classList.add('playing');
-        isPlaying = true;
         setTimeout(function () {
-            snd.pause();
+            padSound.pause();
             pad.classList.remove('playing');
-            isPlaying = false;
         }, 500);
     }
 }
 
-window.addEventListener("load", GameStart);
-
 function GameStart(){
 
 }
+
+window.onload = setTimeout(GameStart, 1000);
