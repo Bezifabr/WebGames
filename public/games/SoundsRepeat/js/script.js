99860816e1eaa70e5d4bb54113105895221a6dfc
Bezifabr <bezifabr@gmail.com>
2020-04-02 16:01:47 +0200

    Add game logic

--- public/games/SoundsRepeat/js/script.js
+++ public/games/SoundsRepeat/js/script.js
@@ -1,9 +1,12 @@
-
+var score = 0;
+var gameOver = false;
+var sequenceQueue = [];
+var secondQueue = [];
 var gameOver = false;
 
 function PlaySound(padId) {
     if (gameOver == false) {
-        var padSound = new Audio("../wav/pad"+padId + ".wav");
+        var padSound = new Audio("../wav/pad" + padId + ".wav");
         var pad = document.getElementById("pad" + padId);
         padSound.currentTime = 0;
         padSound.volume = 0.2;
@@ -19,13 +22,38 @@ function PlaySound(padId) {
 
 function RandomizePad() {
     var padNumber = Math.floor(Math.random() * 4) + 1;
-
-    PlaySound(padNumber);
+    sequenceQueue.push(padNumber);
+    secondQueue = Object.create(sequenceQueue);
+    console.log("Pad" + padNumber + "     secondQueue " + secondQueue.length);
+    setTimeout(function () {
+        PlaySound(padNumber);
+    }, 1000);
 }
 
-function GameStart(){
+function GameStart() {
     RandomizePad();
+}
 
+function GameOver() {
+    gameOver = true;
+}
+
+function CheckPad(padNumber) {
+    if (gameOver == false) {
+        if (secondQueue.length > 0) {
+            var currentPad = secondQueue.shift();
+            if (padNumber != currentPad)
+                GameOver();
+            else
+                PlaySound(currentPad);
+
+            if (gameOver == false && secondQueue.length <= 0) {
+                score++;
+                document.getElementById("scoreLabel").innerHTML = "Score: " + score;
+                RandomizePad();
+            }
+        }
+    }
 }
 
 window.onload = setTimeout(GameStart, 1000);
