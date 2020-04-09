003fba89ac9c8ca18bc16d568cef7629bd9d82c2
Bezifabr <bezifabr@gmail.com>
2020-04-09 18:56:04 +0200

    [Memory] Hide values of cards from the frontend code

--- public/games/Memory/js/script.js
+++ public/games/Memory/js/script.js
@@ -1,5 +1,11 @@
+var values = [1,2,3,1,2,4,5,6,6,3,4,5];
 
-function Flip(elem){
+function Flip(cardId) {
+    var elem = document.querySelector(".card-" + cardId);
     elem.classList.toggle("flip");
 
+    elem.lastElementChild.lastElementChild.innerHTML = "";
+    if (elem.classList.contains("flip")) {
+        elem.lastElementChild.lastElementChild.innerHTML = values[cardId-1];
+    }
 }
