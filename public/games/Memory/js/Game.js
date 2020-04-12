1a87990dbb15ca0d5da7dac793996ca01c4100e1
Bezifabr <bezifabr@gmail.com>
2020-04-12 17:15:59 +0200

    Move Flip() and FlipBack() to separate javascript file

--- public/games/Memory/js/Game.js
+++ public/games/Memory/js/Game.js
@@ -4,35 +4,6 @@ var values = [1,2,3,1,2,4,5,6,6,3,4,5];
 var revealedCardsValues = [];
 var revealedCardsIds = [];
 
-function FlipBack(cardId) {
-    var elem = document.querySelector(".card-" + cardId);
-    elem.classList.remove("flip");
-    elem.lastElementChild.lastElementChild.innerHTML = "";
-}
-
-function Flip(cardId) {
-    var elem = document.querySelector(".card-" + cardId);
-    elem.classList.add("flip");
-    elem.lastElementChild.lastElementChild.innerHTML = values[cardId - 1];
-    revealedCardsIds.push(cardId);
-    revealedCardsValues.push(values[cardId - 1]);
-
-    if (cardsComparator.isReadyToCompare(revealedCardsValues))
-            window.setTimeout(CompareCards, 1000);
-}
-
-function CompareCards() {
-        if(cardsComparator.areCardsMatched(revealedCardsValues)) {
-            for (var i = 0; i < revealedCardsIds.length; i++)
-                document.querySelector(".card-" + revealedCardsIds[i]).remove();
-        } else {
-            for(var i = 0; i < revealedCardsIds.length; i++)
-                FlipBack(revealedCardsIds[i]);
-        }
-
-        revealedCardsIds = [];
-        revealedCardsValues = [];
-}
 
 function GameStart(){
     cardsComparator.setStrategy(new TwoCardsComparator());
