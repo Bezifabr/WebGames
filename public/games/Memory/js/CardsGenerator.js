ad508a7257aa8bc05a25e73802c616c4395be53c
Bezifabr <bezifabr@gmail.com>
2020-04-13 12:53:58 +0200

    [Memory] Generate the cards at the beginning

---
+++ public/games/Memory/js/CardsGenerator.js
@@ -0,0 +1,40 @@
+var CardsGenerator = function () {
+    this.generator = "";
+}
+
+CardsGenerator.prototype = {
+    setStrategy: function(generator){
+        this.generator = generator;
+    },
+
+    generate: function (gameArea) {
+        this.generator.generate(gameArea);
+    }
+}
+
+TwelveCardsGenerator = function () {
+
+    this.generate = function(gameArea){
+         for(var i = 1; i <= 12; i++){
+             var flipper = document.createElement("div");
+             (function(i) {
+                 flipper.onclick = function() {
+                     Flip(i);
+                 }
+             })(i);
+             flipper.classList.add("flipper");
+             flipper.classList.add("card-" + i);
+             var card = document.createElement("div");
+             card.classList.add("card");
+             var front = document.createElement("div");
+             front.classList.add("front");
+             var back = document.createElement("div");
+             back.classList.add("back");
+             card.appendChild(front);
+             card.appendChild(back);
+             flipper.appendChild(card);
+             gameArea.appendChild(flipper);
+         }
+         console.log(gameArea);
+    }
+}
--- public/games/Memory/js/Game.js
+++ public/games/Memory/js/Game.js
@@ -1,4 +1,5 @@
 var cardsComparator = new CardsComparator();
+var cardsGenerator = new CardsGenerator();
 
 var values = [1,2,3,1,2,4,5,6,6,3,4,5];
 var revealedCardsValues = [];
@@ -7,6 +8,8 @@ var revealedCardsIds = [];
 
 function GameStart(){
     cardsComparator.setStrategy(new TwoCardsComparator());
+    cardsGenerator.setStrategy(new TwelveCardsGenerator());
+    cardsGenerator.generate(document.getElementById("gameArea"));
 }
 
 window.onload = GameStart;
